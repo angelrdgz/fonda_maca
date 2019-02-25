a2fab45cfe22f817b5323a3c1186e972df4e9521
Angel Garcia Rodriguez <angelrodriguez@ucol.mx>
2019-02-24 23:58:41 -0600

    styles

--- application/views/layouts/master.php
+++ application/views/layouts/master.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<title>Fonda Maca</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<link rel="stylesheet" href="<?php echo base_url().'assets/css/style.css' ?>">
+	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 	<!--<nav class="nav">
